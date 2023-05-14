30aacd2d92df5ef373f44006a21d7c8aa9001989
alvarosabu <alvaro.saburido@gmail.com>
2023-05-14 12:18:56 +0200

    feat: animated cube

--- src/components/TheExperience.vue
+++ src/components/TheExperience.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
-import { TresCanvas } from '@tresjs/core'
-import { reactive } from 'vue'
-import { SRGBColorSpace } from 'three'
+import { TresCanvas, useRenderLoop } from '@tresjs/core'
+import { reactive, shallowRef } from 'vue'
+import { Mesh, SRGBColorSpace } from 'three'
 import { OrbitControls } from '@tresjs/cientos'
 
 const state = reactive({
   clearColor: '#82DBC5',
   outputColorSpace: SRGBColorSpace,
+})
+
+const boxRef = shallowRef<Mesh>()
+
+const { onLoop } = useRenderLoop()
+
+onLoop(({delta, elapsed}) => {
+  if(boxRef.value) {
+    boxRef.value.rotation.x += delta
+    boxRef.value.rotation.y = elapsed
+  }
 })
 
 </script>
@@ -15,8 +26,8 @@ const state = reactive({
   <TresCanvas v-bind="state">
     <TresPerspectiveCamera :position="[5,5,5]" />
     <OrbitControls />
-    <TresMesh :position="[0,2,0]" :rotation="[Math.PI/4, Math.PI /4, 0]">
-      <TresBoxGeometry />
+    <TresMesh ref="boxRef" :position="[0,2,0]" :rotation="[Math.PI/4, Math.PI /4, 0]">
+      <TresBoxGeometry :args="[1,1,1]" />
       <TresMeshNormalMaterial/>
     </TresMesh>
     <TresAxesHelper />
